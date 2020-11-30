91fee5e41049a28e013b053231af73bef44724fd
Steven <56651529+BigGucciSteve@users.noreply.github.com>
2020-11-30 03:01:02 -0800

    added untracked files

---
+++ Controllers/AuthorWithBooksController.cs
@@ -0,0 +1,39 @@
+﻿using CoreAngCombinedNew.Models;
+using CoreAngCombinedNew.View_Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreAngCombinedNew.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]                            /*ControllerBase*/
+    public class AuthorWithBooksController : Controller{
+        private readonly BookDatabaseContext _context;
+
+        public AuthorWithBooksController(BookDatabaseContext context)
+        {
+            _context = context;
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: api/Authors/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<AuthorWithBooksVM>>> GetAuthorWithBooks(int id)
+        {
+            IQueryable<AuthorWithBooksVM> authorWithBooks = from a in _context.Authors
+                                  where a.AuthorId == id
+                                  select new AuthorWithBooksVM
+                                  {
+                                      AuthorName = a.Name,
+                                      BookNameList = a.Books.Select(b => b.Title).ToList()
+                                  };
+
+            return await authorWithBooks.ToListAsync();
+        }
+    }
+}
---
+++ Models/AuthorWithBooksVM.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoreAngCombinedNew.View_Models
+{
+    public class AuthorWithBooksVM
+    {
+        [Display(Name ="Author Name")]
+        public string AuthorName { get; set; }
+
+        [Display(Name="Book Name List")]
+        public List<string> BookNameList { get; set; }
+
+    }
+}
